1c5f2d1fa3ab635c5b6f886c00d397bd3689c823
Tony Askar <askar.b.tony@gmail.com>
2014-10-04 11:49:03 -0700

    cleaning up code, adding comments

--- photocontroller_node/photocontroller_node.ino
+++ photocontroller_node/photocontroller_node.ino
@@ -1,10 +1,13 @@
 /*
- Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+  Photocontroller transmits instruction to car based on led movement over two
+  photoresistors. 
 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- version 2 as published by the Free Software Foundation.
-//2014 - TMRh20 - Updated along with Optimized RF24 Library fork
+  RF24 transmitter code was based off J. Coliz examples.
+  Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  version 2 as published by the Free Software Foundation.
  */
 
 /**
@@ -23,18 +26,20 @@ RF24 radio(9, 10);
 byte addresses[][6] = {"1Node","2Node"};
 
 // Photoresistors variables 
-int lightPin1 = 2; 
-int lightPin2 = 3;
+int lightPin1 = 0; 
+int lightPin2 = 1;
 double sensorValue1;
 double sensorValue2;
 
 
 void setup() {
 
-  Serial.begin(57600);
-  printf_begin();
-  printf("\n\rRF24/examples/GettingStarted/\n\r");
-  printf("*** PRESS 'T' to begin transmitting to the other node\n\r");
+//  Serial.begin(57600);
+  Serial1.begin(9600);
+//
+//  printf_begin();
+//  printf("\n\rRF24/examples/GettingStarted/\n\r");
+//  printf("*** PRESS 'T' to begin transmitting to the other node\n\r");
 
   // Setup and configure rf radio
   radio.begin();                          // Start up the radio
@@ -46,7 +51,7 @@ void setup() {
   radio.startListening();                 // Start listening
   radio.printDetails();                   // Dump the configuration of the rf unit for debugging
 
-  printf("*** CHANGING TO TRANSMIT ROLE *** \n\r");
+//  printf("*** CHANGING TO TRANSMIT ROLE *** \n\r");
   radio.openWritingPipe(addresses[0]);
   radio.openReadingPipe(1,addresses[1]);
 
@@ -57,30 +62,41 @@ void loop(void){
 
   sensorValue1 = analogRead(lightPin1);
   sensorValue2 = analogRead(lightPin2);
+  Serial.print("sensor 1: ");
+  Serial.println(sensorValue1);
+  Serial.print("sensor 2: ");
+  Serial.println(sensorValue2);
+  Serial.println();
+  
   int dir = find_direction(sensorValue1, sensorValue2);
-  printf("Now sending %d. \n\r", dir);
+//  printf("Now sending %d. \n\r", dir);
 
   if (!radio.write( &dir, sizeof(int) )){  printf("failed.\n\r");  }
   
   // repeat measurement after half a second.
   delay(500);
+//  printf("\n\r");
 }
 
 
 int find_direction(float sensorValue1, float sensorValue2){
   // return values: stop, forward, backward
-  int dir;
+  int return_dir;
   int delta_to_stop = 60;
   float intensity_difference = sensorValue1 - sensorValue2;
+  
   if(abs(intensity_difference) < delta_to_stop){
-    int dir = 0;
+    Serial.println("stop");
+    return_dir = 0;
   }
   else if (sensorValue1 > sensorValue2){
-    int dir = 1;
+    Serial.println("forward");
+    return_dir = 1;  
   }
   else {
-    int dir = 2;
-  }
+    Serial.println("backward");
+    return_dir = 2;  
+  }  
   
-  return dir;
+  return return_dir;
 }
