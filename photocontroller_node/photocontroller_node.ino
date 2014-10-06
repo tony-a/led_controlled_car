a1836e6f3431b8698170c7c3e704f1cfb63a3f67
Tony Askar <askar.b.tony@gmail.com>
2014-10-06 01:05:28 -0700

    adding basic code for turn functionality

--- photocontroller_node/photocontroller_node.ino
+++ photocontroller_node/photocontroller_node.ino
@@ -1,88 +1,82 @@
 /*
-  Photocontroller transmits instruction to car based on led movement over two
-  photoresistors. 
+  adding turn functionality
+  using three photoresistors to follow a light source.
+  
+  Tony Askar
+  10/4/2014
+  link: http://playground.arduino.cc/Learning/PhotoResistor
+  
+ */
 
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  version 2 as published by the Free Software Foundation.  
-
-  RF24 transmitter code was based off J. Coliz examples, Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
-*/
-
-
-#include <SPI.h>
-#include "nRF24L01.h"
-#include "RF24.h"
-#include "printf.h"
-
-// Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 
-RF24 radio(9, 10);
-
-byte addresses[][6] = {"1Node","2Node"};
-
-// Photoresistors variables 
-int lightPin1 = 0;  //define a pin for Photo resistor
-int lightPin2 = 1;
-double sensorValue1;
-double sensorValue2;
-
+int sensorX1 = 0;  //define a pin for Photo resistor
+int sensorX2 = 1;
+int sensorY = 2;
+double valueX1;
+double valueX2;
+double valueY;
 
 void setup(){
-  Serial.begin(57600);
-  
-   // Setup and configure rf radio
-  radio.begin();                          // Start up the radio
-  radio.setAutoAck(1);                    // Ensure autoACK is enabled
-  radio.setRetries(15,15);                // Max delay between retries & number of retries
-  radio.openWritingPipe(addresses[1]);
-  radio.openReadingPipe(1,addresses[0]);
-  
-  radio.startListening();                 // Start listening
-  radio.printDetails();                   // Dump the configuration of the rf unit for debugging
-
-  radio.openWritingPipe(addresses[0]);
-  radio.openReadingPipe(1,addresses[1]);
+  Serial.begin(9600);
 }
-
 
 void loop(){
-  radio.stopListening();                 // First, stop listening so we can talk.
-
-  sensorValue1 = analogRead(lightPin1);
-  sensorValue2 = analogRead(lightPin2);
+  valueX1 = analogRead(sensorX1);  // Top left corner of T
+  valueX2 = analogRead(sensorX2);  // Top right corner of T
+  valueY = analogRead(sensorY);    // Bottom of T
   
-  Serial.print("sensor 1: ");
-  Serial.println(sensorValue1);
-  Serial.print("sensor 2: ");
-  Serial.println(sensorValue2);
+  Serial.print("sensor X1: ");
+  Serial.println(valueX1);
+  Serial.print("sensor X2: ");
+  Serial.println(valueX2);
+  Serial.print("sensor Y: ");
+  Serial.println(valueY);
   Serial.println();
 
-  int dir = find_direction(sensorValue1, sensorValue2);
+  double top_average = (valueX1 + valueX2)/2.0;
+  Serial.print("average: ");
+  Serial.println(top_average);
   
-  Serial.println(dir);
-  if (!radio.write( &dir, sizeof(int) )){  printf("failed.\n\r");  }
+  find_direction(valueY, top_average);
+  find_horizontal_direction(valueX1, valueX2);
+  delay(1000);
+  Serial.println("-----------------");
 
-  delay(200);
 }
 
-int find_direction(float sensorValue1, float sensorValue2){
+void find_direction(float sensorValue1, float sensorValue2){
   // return values: stop, forward, backward
-  int return_dir;
-  int delta_to_stop = 60;
+  int delta_to_stop = 30;
   float intensity_difference = sensorValue1 - sensorValue2;
-  
+	
   if(abs(intensity_difference) < delta_to_stop){
     Serial.println("stop");
-    return_dir = 0;
   }
   else if (sensorValue1 > sensorValue2){
     Serial.println("forward");
-    return_dir = 1;  
   }
   else {
     Serial.println("backward");
-    return_dir = 2;  
-  }  
-  
-  return return_dir;
+  }
+
 }
+
+int find_horizontal_direction(double valueX1, double valueX2){
+  int delta_to_go_straight = 60;
+  double intensity_difference = valueX1 - valueX2;
+  int turn_dir;
+  
+  if (abs(intensity_difference) < delta_to_go_straight) {
+    Serial.println("going straight");
+    turn_dir = 0;
+  }
+  else if (valueX1 < valueX2) {
+    Serial.println("turning left");
+    turn_dir = 1;
+  }
+  else {
+    Serial.println("turning right");
+    turn_dir = 2;
+  }
+  return turn_dir;
+}
+
